5d3e85e8effd6276c3438006873724da74845783
minjor <minjor@minjor.com>
2026-01-14 23:21:12 +0800

    init

---
+++ src/main/java/com/minjor/template_spring_boot/TemplateSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.minjor.template_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateSpringBootApplication.class, args);
+	}
+
+}
